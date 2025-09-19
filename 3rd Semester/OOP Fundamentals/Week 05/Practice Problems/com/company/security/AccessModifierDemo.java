507ec163664ad0ecc36f7b938e59c0cbb5969c60
anish-9387 <anishmall3879@gmail.com>
2025-09-20 00:04:38 +0530

    completed Week 5 codes

--- 3rd Semester/OOP Fundamentals/Week 05/Practice Problems/com/company/security/AccessModifierDemo.java
+++ 3rd Semester/OOP Fundamentals/Week 05/Practice Problems/com/company/security/AccessModifierDemo.java
@@ -59,3 +59,20 @@ public class AccessModifierDemo{
         demo.testInternalAccess();
     }
 }
+
+class SamePackageTest{
+    public static void testAccess(){
+        AccessModifierDemo demo=new AccessModifierDemo(5,"World",15.7,false);
+
+        System.out.println("------ Access from SamePackageTest ------");
+        // System.out.println(demo.privateField);
+        System.out.println(demo.defaultField);
+        System.out.println(demo.protectedField);
+        System.out.println(demo.publicField);
+
+        // demo.privateMethod();
+        demo.defaultMethod();
+        demo.protectedMethod();
+        demo.publicMethod();
+    }
+}
