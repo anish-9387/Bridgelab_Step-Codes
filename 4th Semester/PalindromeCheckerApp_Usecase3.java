3a08247a40d9d9249f1b8e95505afa336dd3720b
anish-9387 <anishmall3879@gmail.com>
2026-02-23 15:30:32 +0530

    completed usecases 3 and 4 of Palindrome Checker App

---
+++ 4th Semester/PalindromeCheckerApp_Usecase3.java
@@ -0,0 +1,36 @@
+/**
+ * ==========================================================
+ * MAIN CLASS – PalindromeCheckerApp_Usecase3
+ * ==========================================================
+ *
+ * Use Case 3: Reverse String Based Palindrome Check
+ *
+ * Description:
+ * This class checks whether a string is a palindrome
+ * by reversing the string and comparing it with
+ * the original value.
+ *
+ * @author Anish
+ * @version 3.0
+ */
+
+public class PalindromeCheckerApp_Usecase3{
+    public static void main(String[] args) {
+        String input="madam";
+
+        System.out.println("Input text: "+input);
+
+        String reversed="";
+
+        for(int i=input.length()-1; i>=0; i--){
+            reversed=reversed+input.charAt(i);
+        }
+
+        if(input.equals(reversed)){
+            System.out.println("Is it a Palindrome? : true");
+        }
+	else{
+            System.out.println("Is it a Palindrome? : false");
+        }
+    }
+}
