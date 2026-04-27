1834e5bfa890e5de6f0edaa483ce04d9a8501d30
anish-9387 <anishmall3879@gmail.com>
2026-04-28 00:11:00 +0530

    Completed UC 17 of TrainConsistentManagementApp

--- 4th Semester/App/TrainConsistentManagementApp.java
+++ 4th Semester/App/TrainConsistentManagementApp.java
@@ -1,36 +1,18 @@
 import java.util.*;
 
 public class TrainConsistentManagementApp{
-    public static void bubbleSort(int[] arr){
-        int n=arr.length;
-
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-        }
-    }
-
-    public static void printArray(int[] arr){
-        for(int val : arr){
-            System.out.print(val+" ");
-        }
-        System.out.println();
-    }
+    public static void sortBogieNames(String[] bogieNames){
+        Arrays.sort(bogieNames);    }
 
     public static void main(String[] args){
-        int[] capacities={72, 56, 24, 70, 60};
+        String[] bogies={"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
         System.out.println("Before Sorting:");
-        printArray(capacities);
+        System.out.println(Arrays.toString(bogies));
 
-        bubbleSort(capacities);
+        sortBogieNames(bogies);
 
         System.out.println("After Sorting:");
-        printArray(capacities);
+        System.out.println(Arrays.toString(bogies));
     }
 }
